4dccb1cf21234fd35ff506caf2d8308855737bac
lirik24 <fotolirika@gmail.com>
2024-05-11 01:00:34 +0300

    #3 DinTipizaciya.py

--- #3 DinTipizaciya.py
+++ #3 DinTipizaciya.py
@@ -1,8 +1,8 @@
-name='Kirill'
+name = 'Kirill'
 print('Name:',name)
-age=32
+age = 32
 print('Age:',age)
-age=age+1
+age = age + 1
 print('New age:',age)
 is_student = bool
 print('Is student:',bool(is_student))
